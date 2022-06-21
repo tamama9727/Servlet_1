35a052ad1fbc4b55349a4aad43e9013c3c3b9b46
DESKTOP-O403M3T\user <jungu2008@naver.com>
2022-06-21 21:42:13 +0900

    [권성진] -servlet quiz10 작업중 추가 -22년 06월 21일 학원에서 최종업로드

---
+++ src/main/java/com/marondal/servlet/quiz/ServletQuiz10Controller.java
@@ -0,0 +1,41 @@
+package com.marondal.servlet.quiz;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/quiz10")
+public class ServletQuiz10Controller extends HttpServlet{
+	
+	private final Map<String, String> userMap =  new HashMap<String, String>() {
+	    {
+	        put("id", "hagulu");
+	        put("password", "asdf");
+	        put("name", "김인규");
+	    }
+	};
+	
+	@Override
+	public void doPost(HttpServletRequest request , HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		out.println("<html> <head> </head> <body>");
+		out.println("<h1>" + "</h1>");
+		out.println("</body> </html>");
+	}
+	
+
+
+}
+
