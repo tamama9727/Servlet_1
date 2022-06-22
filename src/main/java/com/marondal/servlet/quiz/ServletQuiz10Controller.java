e08cb7ee342985e07d3c2023af16297d7544b7da
DESKTOP-O403M3T\user <jungu2008@naver.com>
2022-06-22 19:19:31 +0900

    [권성진] -quiz10 미완성본 추가 -수정바람

--- src/main/java/com/marondal/servlet/quiz/ServletQuiz10Controller.java
+++ src/main/java/com/marondal/servlet/quiz/ServletQuiz10Controller.java
@@ -30,8 +30,15 @@ public class ServletQuiz10Controller extends HttpServlet{
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		
-		out.println("<html> <head> </head> <body>");
-		out.println("<h1>" + "</h1>");
+		out.println("<html> <head> <title> 로그인 </title> </head> <body>");
+		// id가 일치하지 않을때
+		if(!id.equals(userMap.get("id"))) {
+			out.println("<h1>아이디가 일치하지 않습니다.</h1>");
+		}else if(!password.equals(userMap.get("password"))){//password가 일치하지 않을때
+			out.println("<h1> 비밀번호가 일치하지 않습니다.</h1>");
+		}else {			
+			out.println("<h1>" + userMap.get("name") + "님 환영합니다.</h1>");
+		}
 		out.println("</body> </html>");
 	}
 	
